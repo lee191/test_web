f50acd1d5baabf894563e1ffa9840e57bf854512
lee191 <lsj93219321@gmail.com>
2024-03-11 14:50:18 +0900

    test2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello World! My name is Lee SungJin and My age is 26</p>
+        <p>Hello World! My name is Lee SungJin</p>
       </header>
     </div>
   );
